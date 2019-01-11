28c9b93af19d97ac69fe31a084271d0d063d9469
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-11 11:06:14 -0800

    added api keys to .env

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -49,23 +49,22 @@ module.exports = function (app) {
 
 };
 
-
 module.exports = function (app) {
-  // Get all reviews
+  // GET route for getting all of the reviews
   app.get("/api/reviews", function (req, res) {
     db.Review.findAll({}).then(function (dbReviews) {
       res.json(dbReviews);
     });
   });
 
-  // Create a new reviews
+  // Create a new review
   app.post("/api/reviews", function (req, res) {
     db.Review.create(req.body).then(function (dbReview) {
       res.json(dbReview);
     });
   });
 
-  // Delete an reviews by id
+  // Delete a review by id
   app.delete("/api/reviews/:id", function (req, res) {
     db.Review.destroy({ where: { id: req.params.id } }).then(function (dbReview) {
       res.json(dbReview);
