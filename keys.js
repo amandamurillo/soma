ce7684661fa7dc81175b30f23d6927326e05cff0
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-11 13:53:50 -0800

    spotify & bands in town api key

---
+++ keys.js
@@ -0,0 +1,8 @@
+var keys = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET,
+    omdb: process.env.OMDB_APIKEY,
+    bands: process.env.BANDS_APIKEY
+}
+
+module.exports = keys;
