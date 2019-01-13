7967e3728769bf6237f7043d6e409baf82cfb59e
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-13 15:09:34 -0800

    user login, user logout are all working. once user is logged in it will take the user to their profile page. wasn't able to test the search nav bar yet due to merge conflicts, but testing out the spotify api and it is working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,35 @@
 var db = require("../models");
 var path = require("path");
+var artistUrl;
+var Spotify = require("node-spotify-api");
+var keys = require("../keys");
+var spotify = new Spotify(keys);
+
 module.exports = function (app) {
 
+
+  app.post("/search-spotify", function (req, res) {
+    var artistSearch = req.body.artist.trim();
+
+    spotify.search({ type: "artist", query: artistSearch, limit: 1 }, function (err, data) {
+      // Error handler
+      if (err) {
+        return console.log(err);
+      }
+
+      // console.log(data.artists.items[0].href);
+      artistUrl = data.artists.items[0].href;
+      res.redirect("/artist");
+
+    });
+
+    // res.redirect("/");
+  });
+
   // Load index page
-  app.get("/index", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get("/", function (req, res) {
+    // res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("../views/layouts/main");
   });
 
   // Load create account page
@@ -12,21 +37,20 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/create.html"));
   });
 
-  // Load login page
+  // Load signIn page
 
-  app.get("/login", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"))
+  app.get("/signIn", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/signIn.html"))
   })
 
-  // Load signIn page
-  app.get("/lemmesee", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/signIn.html"));
+  // Load user profile page
+  app.get("/user", function (req, res) {
+    res.render("user", { test: "works" });
   });
-
 
   app.get("/", function (req, res) {
     db.Example.findAll({}).then(function (dbExamples) {
-      res.render("index", {
+      res.render("main", {
         msg: "Welcome!",
         examples: dbExamples
       });
@@ -34,8 +58,10 @@ module.exports = function (app) {
   });
 
   app.get("/artist", function (req, res) {
-    res.render("artist");
+    res.render("artist", { spotifyArtist: artistUrl });
   })
+
+
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
